379b915a5dd4463d1341c39be119270a890278fb
Szymon Lato <szymek.lato@gmail.com>
2026-01-11 20:41:10 +0100

    skrypt ułatwiający dodanie pierwszego admina

---
+++ scripts/addFirstAdmin.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const User = require('../models/User');
+
+const dbConnData = {
+    host: process.env.MONGO_HOST || '127.0.0.1',
+    port: process.env.MONGO_PORT || 27017,
+    database: process.env.MONGO_DATABASE || 'ProgTalk'
+}
+
+const plantAnAdmin = async () => {
+    try {
+        await mongoose.connect(`mongodb://${dbConnData.host}:${dbConnData.port}/${dbConnData.database}`);
+        const adminExists = await User.findOne({role: 'admin'});
+
+        if (adminExists) {
+            console.log("Jakiś admin już istnieje");
+            process.exit(1);
+        }
+
+        const hash = await bcrypt.hash('adminnimda', 12);
+        const admin = new User({
+            mail: "admin@first.pl",
+            login: 'admin',
+            hash,
+            role: 'admin',
+            isApprovedByAdmin: true,
+            isBlocked: false
+        });
+
+        await admin.save();
+        console.log("Admin dodany", admin);
+        process.exit(0);
+    } catch (error) {
+        throw new Error("Coś poszło nie tak przy dodawaniu admina");
+        process.exit(1)
+    }
+}
+
+plantAnAdmin();
