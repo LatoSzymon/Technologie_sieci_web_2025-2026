9d3fa6e488a836f5c4f875aa7cde036911be87e6
Szymon Lato <szymek.lato@gmail.com>
2026-01-22 22:06:40 +0100

    dodanie pełnego pliku z routes dla tagów

--- routes/tagRoutes.js
+++ routes/tagRoutes.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
-const {addTag, deleteTag, updateTag } = require("../controllers/tagControllers");
+const {addTag, deleteTag, updateTag, getAllTags, getTagById } = require("../controllers/tagControllers");
+const { isAdmin } = require('../middleware/authMiddleware');
 const passport = require("../passport");
 
+router.use(passport.authenticate('jwt', { session: false }));
+
+router.get("/", getAllTags);
+router.get("/:id", getTagById);
+router.post("/addTag", isAdmin, addTag);
+router.delete("/:tagId", isAdmin, deleteTag);
+router.put("/:tagId", isAdmin, updateTag);
+
+
+module.exports = router;
