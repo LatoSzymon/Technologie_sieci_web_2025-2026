b4adcb6013b95163bc166293c928fb2abfe515cc
Szymon Lato <szymek.lato@gmail.com>
2026-01-22 22:46:25 +0100

    Dodanie middleware do sprawdzania czy użytkownik jest zaakceptowany przez admina

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,35 +2,35 @@ require("dotenv").config();
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
+//Useless?
+// const auth = (req, res, next) => {
+//     try {
+//         let token = req.cookies.jwt;
 
-const auth = (req, res, next) => {
-    try {
-        let token = req.cookies.jwt;
+//         if (!token && req.headers.authorization) {
+//             const authHeader = req.headers.authorization;
 
-        if (!token && req.headers.authorization) {
-            const authHeader = req.headers.authorization;
+//             if (authHeader.startsWith('Bearer ')) {
+//                 token = authHeader.substring(7);
+//             }
+//         }
 
-            if (authHeader.startsWith('Bearer ')) {
-                token = authHeader.substring(7);
-            }
-        }
+//         if (!token) {
+//             return res.status(401).json({ message: "Brak tokenu uwierzytelniającego. Dostęp zablokowany"});
+//         }
 
-        if (!token) {
-            return res.status(401).json({ message: "Brak tokenu uwierzytelniającego. Dostęp zablokowany"});
-        }
+//         const verified = jwt.verify(token, proccess.env.JWT_SECRET);
 
-        const verified = jwt.verify(token, proccess.env.JWT_SECRET);
+//         req.user = {
+//             userId: decoded.userId,
+//             role: decoded.role
+//         };
 
-        req.user = {
-            userId: decoded.userId,
-            role: decoded.role
-        };
-
-        next();
-    } catch (error) {
-        return res.status(500).json({message: "Błąd przy autoryzacji tokenu", error});
-    }
-};
+//         next();
+//     } catch (error) {
+//         return res.status(500).json({message: "Błąd przy autoryzacji tokenu", error});
+//     }
+// };
 
 const isAdmin = (req, res, next) => {
     if (req.user?.role !== 'admin') {
@@ -39,4 +39,22 @@ const isAdmin = (req, res, next) => {
     next();
 }
 
-module.exports = {auth, isAdmin};
+const isApproved = async (req, res, next) => {
+    try {
+        const id = req.user.userId;
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(401).json({ message: "Nie znaleziono użytkownika" });
+        }
+        if (!user.isApprovedByAdmin) {
+            return res.status(403).json({ message: "Konto nie zostało jeszcze zaakceptowane przez administratora" });
+        }
+
+        next();
+    } catch (err) {
+        return res.status(500).json({message: "Nie udało się uwierzytelnić uzytkownika", err});
+    }
+}
+
+module.exports = {isAdmin, isApproved};
