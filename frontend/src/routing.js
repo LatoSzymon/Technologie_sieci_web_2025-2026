f40827a330a6d3c5ba60591bf37e13ca9a939359
Szymon Lato <szymek.lato@gmail.com>
2026-01-24 19:38:09 +0100

    tworzenie komponentow i routing

---
+++ frontend/src/routing.js
@@ -0,0 +1,14 @@
+import {createRouter, createWebHistory} from "vue-router";
+import Login from './components/Login.vue'
+import Register from './components/Register.vue'
+import Topics from './components/Topics.vue'
+import Chat from './components/Chat.vue'
+import TopicView from "./components/TopicView.vue";
+
+const routes = [
+  { path: '/', component: Topics },
+  { path: '/login', component: Login },
+  { path: '/register', component: Register },
+  { path: '/topics/:id', component: TopicView },
+  { path: '/chat', component: Chat }
+];
