f65451dcf1ad1d6651bf93dcf93fa83507eb3b66
Szymon Lato <szymek.lato@gmail.com>
2026-01-27 01:59:37 +0100

    zalegle zmiany w sciezkach dla uzytkownikow

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
-const {register, login, logout, getMe} = require('../controllers/usersController');
+const {register, login, logout, getMe, updateProfile, changePassword} = require('../controllers/usersController');
 const passport = require('../passport');
 const { isApproved } = require('../middleware/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get("/me", passport.authenticate('jwt', { session: false }), getMe);
-router.post('/logout', passport.authenticate('jwt', { session: false }), logout)
+router.post('/logout', passport.authenticate('jwt', { session: false }), logout);
+router.put('/profile', passport.authenticate('jwt', { session: false }), updateProfile);
+router.post('/change-password', passport.authenticate('jwt', { session: false }), changePassword);
 
 module.exports = router;
