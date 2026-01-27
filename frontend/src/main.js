c3b5399323f521a8c19ec9ac170ce704e23a916b
Szymon Lato <szymek.lato@gmail.com>
2026-01-27 02:10:55 +0100

    dodanie highight do main we frontendzie

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,24 @@
 import { createApp } from 'vue';
 import './style.css';
+import 'highlight.js/styles/atom-one-dark.css';
 import App from './App.vue';
 import router from './routing';
 import { createPinia } from 'pinia';
+import hljs from 'highlight.js';
 
-createApp(App).use(createPinia()).use(router).mount('#app');
+const app = createApp(App);
+
+app.directive('highlight', {
+  mounted(el) {
+    if (el.querySelector('code')) {
+      hljs.highlightElement(el.querySelector('code'));
+    }
+  },
+  updated(el) {
+    if (el.querySelector('code')) {
+      hljs.highlightElement(el.querySelector('code'));
+    }
+  }
+});
+
+app.use(createPinia()).use(router).mount('#app');
